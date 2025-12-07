1ee2a6c245e68fed377dd007bc95cb478e2be185
yahiab05 <yahiaboukharrata092@gmail.com>
2025-12-07 12:37:50 +0100

    finalizing chunker

--- backend/app/services/chuncker.py
+++ backend/app/services/chuncker.py
@@ -1,46 +1,26 @@
-from sentence_transformers import SentenceTransformer, util
-import nltk
-from langchain_core.documents import Document
+from langchain_text_splitters import RecursiveCharacterTextSplitter
+from langchain_text_splitters import SentenceTransformersTokenTextSplitter
 
 class Chunker:
     def __init__(self):
-        self.model = SentenceTransformer('all-MiniLM-L6-v2')
-        nltk.download('punkt')
+        self.sementic_splitter = SentenceTransformersTokenTextSplitter(
+            sentence_transformer_model_name="all-MiniLM-L6-v2",
+            chunk_size=500,
+            chunk_overlap=150
+        )
+        self.recursive_splitter = RecursiveCharacterTextSplitter(
+            chunk_size=500, 
+            chunk_overlap=150
+            )
         
-    def chunck_split(self, docs, threshold = 0.6):
-        text, metadata = self.extract_text(docs)
+    def chunck(self, docs, sementic = True):
+        if sementic:
+            return self.chunck_sementic(docs)
+        else:
+            return self.chunck_simple(docs)
         
-        sentences = nltk.sent_tokenize(text)
-        
-        embeddings = self.model.encode(sentences, convert_to_tensor=True)
-        
-        chunks = []
-        current_chunks = [sentences[0]]
-        
-        for i in range(1, len(sentences)):
-            similarity = util.cos_sim(embeddings[i], embeddings[i - 1]) 
-            if similarity > threshold:
-                current_chunks.append(sentences[i])
-            else:
-                chunks.append(current_chunks)
-                current_chunks = [sentences[i]]
-        
-        if current_chunks:
-            chunks.append(current_chunks)
-        
-        chunks = self.restore_docs(chunks, metadata)
-        return chunks
+    def chunk_sementic(self, docs):
+        return self.model.split_documents(docs)
     
-    def extract_text(self, doc: list):
-        for d in doc:
-            text += " " + d.page_content
-            
-        return text, doc[0].metadata
-    
-    def restore_docs(self, chunks:list, metadata):
-        docs = []
-        for chunk in chunks:
-            docs.append(Document(
-                page_content=chunk, 
-                metadata=metadata))
-        return docs
+    def chunk_simple(self, docs):
+        return self.recursive_splitter.split_documents(docs)
