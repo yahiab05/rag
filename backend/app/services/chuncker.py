177b6764ad90139e9c18d6a98705674cad7dc8e1
yahiab05 <yahiaboukharrata092@gmail.com>
2025-12-15 22:56:37 +0100

    pipeline time optimization

--- backend/app/services/chuncker.py
+++ backend/app/services/chuncker.py
@@ -1,63 +1,14 @@
-from langchain_text_splitters import RecursiveCharacterTextSplitter, SentenceTransformersTokenTextSplitter
+from langchain_text_splitters import RecursiveCharacterTextSplitter
 from langchain_ollama import ChatOllama
 import json
 
 
 class Chunker:
     def __init__(self):
-        self.sementic_splitter = SentenceTransformersTokenTextSplitter(
-            chunk_size=500,
-            chunk_overlap=150
-        )
         self.recursive_splitter = RecursiveCharacterTextSplitter(
-            chunk_size=500, 
-            chunk_overlap=150
+            chunk_size=800, 
+            chunk_overlap=100
             )
-        self.llm = ChatOllama(
-            model="mistral",
-            temperature=0
-        )
-        
-        
-    def chunk(self, docs, sementic = True):
-        tagged_docs = self.add_metadata(docs)
-        if sementic:
-            return self.chunk_sementic(tagged_docs)
-        else:
-            return self.chunk_simple(tagged_docs)
-        
-    def chunk_sementic(self, docs):
-        return self.sementic_splitter.split_documents(docs)
     
-    def chunk_simple(self, docs):
+    def chunk(self, docs):
         return self.recursive_splitter.split_documents(docs)
-    
-    def add_metadata(self, docs):
-        batch = [d.page_content for d in docs]
-        metadata_list = self._extract_metadata_batch(batch)
-        for doc, meta in zip(docs, metadata_list):
-            doc.metadata = meta
-        return docs
-
-    def _extract_metadata_batch(self, texts):
-        prompt = f"""
-        You MUST return a JSON array.
-        Each element MUST be an object with:
-        - title (string)
-        - keywords (array of strings)
-        - description (string)
-
-        Return ONLY a JSON array.
-
-        Texts:
-        {json.dumps(texts)}
-        """
-
-
-        response = self.llm.invoke(prompt).content
-        print(type(response))
-        printer = True
-        if printer:
-            print(response)
-            printer = False
-        return json.loads(response)
--- backend/app/services/tests/test_chunker.py
+++ backend/app/services/tests/test_chunker.py
@@ -1,16 +1,26 @@
 from ..chuncker import Chunker
 from langchain_core.documents import Document
 
-def test_chunker_splits_docs():
-    docs = [
-        Document(page_content="this is a long document that should be chunked.")
-        ]
-    chunker = Chunker()
+docs = [
+    Document(page_content="In a quiet city that never truly slept, people moved through their routines with a mix of intention and habit, rarely stopping to notice how many small decisions shaped their days. Conversations floated through cafés, offices, and buses, blending plans for the future with reflections on the past, while screens glowed softly in hands and pockets. Somewhere between work deadlines and personal dreams, moments of curiosity appeared, brief but meaningful, pushing individuals to learn, create, or question what they already knew. Technology accelerated these moments, connecting distant ideas and people in seconds, yet it also reminded everyone of the importance of slowing down and thinking clearly. Progress did not arrive all at once; it emerged from repeated effort, quiet persistence, and the willingness to improve something just a little each time. In that steady rhythm, growth became less about sudden breakthroughs and more about consistency, patience, and understanding the value of each step forward.")
+    ]
 
+chunker = Chunker()
+
+def test_chunker_splits_docs():
     chunks = chunker.chunk(docs)
-    assert isinstance(chunks, list)
-    assert len(chunks) > 0  # depending on your logic
+    assert isinstance(chunks, list), show_result(f"Expected type: list, Result: {type(chunks)}")
+    assert len(chunks) > 0 and len(chunks) < 3, show_result(f"Expected Range: (1, 2), Result: {len(chunks)}")
 
 def test_chunk_size():
-    chunker = Chunker()
-    assert chunker.sementic_splitter._chunk_size == 500
+    chunks = chunker.chunk(docs)
+    assert len(chunks[0].page_content) > 0 and len(chunks[0].page_content) < 800, show_result(f"Expected Range: (1, 800), Result: {len(chunks[0].page_content)}")
+    
+def test_chunk_type():
+    chunks = chunker.chunk(docs)    
+    assert isinstance(chunks[0], Document), show_result(f"Expected type: Document, Result: {type(chunks[0])}") 
+
+def show_result(expected, result):
+    return (f"""Expected: {expected} \n
+    Result: {result}""")
+
